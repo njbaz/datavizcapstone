5463188ebab056b1ffc39cfba9bcee305fb8d1b7
Nicole Baz <nicolebaz@nicoles-mbp.mynetworksettings.com>
2024-12-07 16:31:58 -0500

    update 12/7

---
+++ files/timelinescript.js
@@ -0,0 +1,131 @@
+"use strict";
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll",DisplayContent);
+DisplayContent();
+
+function DisplayContent(){
+    const TriggerBottom = (window.innerHeight / 5)*4;
+   
+    boxes.forEach((box)=>{
+        const topBox = box.getBoundingClientRect().top;
+
+        if(topBox<TriggerBottom){
+            box.classList.add("show")
+        }
+        else{
+            box.classList.remove("show");
+        }
+    });
+}
+
+
+//button opens and closes//
+//event 1 button
+const openBtn1 = document.getElementById("openModal1");
+const closeBtn1 = document.getElementById("closeModal1");
+const modal = document.getElementById("modal1");
+
+openBtn1.addEventListener("click", () => {
+    modal1.classList.add("open");
+});
+
+closeBtn1.addEventListener("click", () => {
+    modal1.classList.remove("open");
+});
+
+//event 2 button
+const openBtn2 = document.getElementById("openModal2");
+const closeBtn2 = document.getElementById("closeModal2");
+const modal2 = document.getElementById("modal2");
+
+openBtn2.addEventListener("click", () => {
+    modal2.classList.add("open");
+});
+
+closeBtn2.addEventListener("click", () => {
+    modal2.classList.remove("open");
+});
+
+
+//event 3 button
+const openBtn3 = document.getElementById("openModal3");
+const closeBtn3 = document.getElementById("closeModal3");
+const modal3 = document.getElementById("modal3");
+
+openBtn3.addEventListener("click", () => {
+    modal3.classList.add("open");
+});
+
+closeBtn3.addEventListener("click", () => {
+    modal3.classList.remove("open");
+});
+
+//event 4 button
+const openBtn4 = document.getElementById("openModal4");
+const closeBtn4 = document.getElementById("closeModal4");
+const modal4 = document.getElementById("modal4");
+
+openBtn4.addEventListener("click", () => {
+    modal4.classList.add("open");
+});
+
+closeBtn4.addEventListener("click", () => {
+    modal4.classList.remove("open");
+});
+
+
+//event 5 button
+const openBtn5 = document.getElementById("openModal5");
+const closeBtn5 = document.getElementById("closeModal5");
+const modal5 = document.getElementById("modal5");
+
+openBtn5.addEventListener("click", () => {
+    modal5.classList.add("open");
+});
+
+closeBtn5.addEventListener("click", () => {
+    modal5.classList.remove("open");
+});
+
+
+//event 6 button
+const openBtn6 = document.getElementById("openModal6");
+const closeBtn6 = document.getElementById("closeModal6");
+const modal6 = document.getElementById("modal6");
+
+openBtn6.addEventListener("click", () => {
+    modal6.classList.add("open");
+});
+
+closeBtn6.addEventListener("click", () => {
+    modal6.classList.remove("open");
+});
+
+
+//event 7 button
+const openBtn7 = document.getElementById("openModal7");
+const closeBtn7 = document.getElementById("closeModal7");
+const modal7 = document.getElementById("modal7");
+
+openBtn7.addEventListener("click", () => {
+    modal7.classList.add("open");
+});
+
+closeBtn7.addEventListener("click", () => {
+    modal7.classList.remove("open");
+});
+
+//event 8 button
+const openBtn8 = document.getElementById("openModal8");
+const closeBtn8 = document.getElementById("closeModal8");
+const modal8 = document.getElementById("modal8");
+
+openBtn8.addEventListener("click", () => {
+    modal8.classList.add("open");
+});
+
+closeBtn8.addEventListener("click", () => {
+    modal8.classList.remove("open");
+});
